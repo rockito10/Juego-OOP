dbe92f4de0c849076da5d5e9387cf3c38cfef7b4
rockito10 <rockito10.mfd@gmail.com>
2024-05-08 12:00:33 -0300

    Player hurt map color

--- entities/Player.py
+++ entities/Player.py
@@ -7,6 +7,12 @@ from entities.GeneralEntities import Entity
 from weapons.Weapons import WoodenMurasama
 from resources.CardinalDirection import North, West, South, East
 from sys import exit
+import os
+import time
+def cls(): #borra la consola
+        os.system('cls' if os.name=='nt' else 'clear')
+
+CLR_RST = "\033[0m"
 
 class Player(Entity):
     def __init__(self):
@@ -18,6 +24,7 @@ class Player(Entity):
         self.map = None
         self.weapon = WoodenMurasama()
         self.looking_at = North()
+        self.screen_color = CLR_RST
         
         
     # def set_current_weapon(self, weapon):
@@ -62,8 +69,22 @@ class Player(Entity):
     def get_room_number_position(self): #getter número de habitación
         return self.current_room_number
     
+    def set_screen(self, color):
+        self.screen_color = color
+        
+    def reset_screen(self):
+        self.screen_color = CLR_RST
+    
     def show_screen(self): #muestra la pantalla (el mapa de la sala)
-        self.current_room.map()
+        self.current_room.map(self.screen_color)
+        self.reset_screen()
+        # if self.screen_color == "\033[91m":
+        #     time.sleep(0.1)
+        #     cls()
+        #     self.reset_screen()
+        #     self.show_screen()
+        
+        
         # print(self.weapon.base_damage())
 
     #movimientos del jugador
@@ -72,22 +93,26 @@ class Player(Entity):
         # self.current_room.move_player__to_next_room_up(self, self.current_floor)
         self.current_room.move_player__to_next_cuadricule_north()
         self.looking_at = North()
+        self.show_screen()
 
     def move_left(self):
         # self.current_room.move_player__to_next_room_left(self, self.current_floor)
         self.current_room.move_player__to_next_cuadricule_west()
         self.looking_at = West()
+        self.show_screen()
         # print(self.current_cuadricule_number)
 
     def move_down(self):
         # self.current_room.move_player__to_next_room_down(self, self.current_floor)
         self.current_room.move_player__to_next_cuadricule_south()
         self.looking_at = South()
+        self.show_screen()
 
     def move_right(self):
         # self.current_room.move_player__to_next_room_right(self, self.current_floor)
         self.current_room.move_player__to_next_cuadricule_east()
         self.looking_at = East()
+        self.show_screen()
         
     def move__to_next_room_north(self):
         self.current_floor.move_player__to_next_room_north(self)
@@ -119,6 +144,8 @@ class Player(Entity):
         self.weapon.damage_entity(entity)
         
     def take_damage_from(self, entity):
-        print(entity)
+        # print(entity)
+        red = "\033[91m"
         self.take_damage(entity.damage)
         print(self.hp)
+        self.set_screen(red)
